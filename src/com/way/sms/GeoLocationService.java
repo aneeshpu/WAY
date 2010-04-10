89fcd7101cf4c4d4d5b5096dd122868adb804e73
Aneesh P U <aneeshpu@gmail.com>
2010-04-10 15:15:36 +0530

    some invain refactoring in the hope that I could get gps locations :(

--- src/com/way/sms/GeoLocationService.java
+++ src/com/way/sms/GeoLocationService.java
@@ -6,25 +6,20 @@ import android.content.Context;
 import android.location.Address;
 import android.location.Location;
 import android.location.LocationManager;
+import android.util.Log;
 
 public class GeoLocationService {
 
 	private MyGeoCoder myGeoCoder;
-	
-	public GeoLocationService(MyGeoCoder myGeoCoder){
+
+	public GeoLocationService(MyGeoCoder myGeoCoder) {
 		this.myGeoCoder = myGeoCoder;
-		
+
 	}
 
 	public GeoLocation getCurrentGeoLocation(Context context) {
-
 		final LocationManager locationService = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-		final List<String> allProviders = locationService.getAllProviders();
-//		System.setProperty("log.tag.WAY", "DEBUG");
-		
-		return askGPS(context, locationService);
-//		return askAllProviders(context, locationService, allProviders);
-		
+		return askAllProviders(context, locationService, locationService.getAllProviders());
 	}
 
 	private GeoLocation askGPS(Context context, LocationManager locationService) {
@@ -34,24 +29,28 @@ public class GeoLocationService {
 	private GeoLocation askAllProviders(Context context, final LocationManager locationService, final List<String> allProviders) {
 		for (String provider : allProviders) {
 
-//			Log.d("way", String.format("---provider:%s",provider));
-			
 			final GeoLocation lastKnownGeoLocation = askProvider(context, locationService, provider);
-			if(lastKnownGeoLocation != null)return lastKnownGeoLocation;
+			if (lastKnownGeoLocation != null)
+				return lastKnownGeoLocation;
 		}
-		
+
 		return GeoLocation.Null;
 	}
 
 	private GeoLocation askProvider(Context context, final LocationManager locationService, String provider) {
+		Log.d("way", String.format("---provider:%s", provider));
+
 		final Location lastKnownLocation = locationService.getLastKnownLocation(provider);
+		if (lastKnownLocation == null)
+			return null;
 		final List<Address> fromLocation = myGeoCoder.getFromLocation(context, lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude(), 1);
 
 		final String address = makeAddress(fromLocation);
-//			Log.d("way", address);
+
+		// Log.d("way", address);
 		if (address != null)
 			return new GeoLocation(address, provider);
-		
+
 		return null;
 	}
 
