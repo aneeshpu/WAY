c9405e973a3e4896f8f66686402812bcbd0682da
Aneesh P U <aneeshpu@gmail.com>
2010-06-20 11:23:46 +0530

    Removed chain handling from RegularTextMessage

--- src/com/way/sms/RegularTextMessage.java
+++ src/com/way/sms/RegularTextMessage.java
@@ -1,29 +1,22 @@
 package com.way.sms;
 
-
 import android.telephony.SmsMessage;
 
-class RegularTextMessage implements TextMessage{
-	
+class RegularTextMessage implements TextMessage {
+
 	private final SmsMessage smsMessage;
 	private final Locator locator;
 	private WayRequest wayRequest;
-	private final TextMessage textMessageHandler;
 
-	public RegularTextMessage(android.telephony.SmsMessage smsMessage, Locator locator, WayRequest wayRequest, TextMessage textMessageHandler) {
+	public RegularTextMessage(android.telephony.SmsMessage smsMessage, Locator locator, WayRequest wayRequest) {
 		this.smsMessage = smsMessage;
 		this.locator = locator;
 		this.wayRequest = wayRequest;
-		this.textMessageHandler = textMessageHandler;
 	}
 
 	public boolean isWayRequest() {
 		final String messageBody = smsMessage.getMessageBody();
-		return wayRequest.isWayRequest(messageBody) || delegateToNextHandler();
-	}
-
-	private boolean delegateToNextHandler() {
-		return textMessageHandler != null && textMessageHandler.isWayRequest();
+		return wayRequest.isWayRequest(messageBody);
 	}
 
 	@Override
--- src/com/way/sms/WaySMSFactory.java
+++ src/com/way/sms/WaySMSFactory.java
@@ -5,7 +5,7 @@ import android.content.Context;
 public class WaySMSFactory {
 
 	public RegularTextMessage create(Object[] pdus, Context context) {
-		return new RegularTextMessage(android.telephony.SmsMessage.createFromPdu((byte[]) pdus[0]), new Locator(new MyGeoCoder(), context), new WayRequest(), null);
+		return new RegularTextMessage(android.telephony.SmsMessage.createFromPdu((byte[]) pdus[0]), new Locator(new MyGeoCoder(), context), new WayRequest());
 	}
 
 }
